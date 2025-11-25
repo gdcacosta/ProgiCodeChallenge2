d78bac78b634b676b48ef5689dde58d9bde1e58c
German Acosta <germandanielacosta@outlook.com>
2025-11-24 22:43:15 -0500

    Remove data annotations

--- backend/src/BidCalculator.Application/Contracts/BidCalculationRequest.cs
+++ backend/src/BidCalculator.Application/Contracts/BidCalculationRequest.cs
@@ -4,5 +4,5 @@ using System.ComponentModel.DataAnnotations;
 namespace BidCalculator.Application.Contracts;
 
 public sealed record BidCalculationRequest(
-    [Required, Range(0.01, 1000000)] decimal BasePrice,
-    [Required] VehicleType VehicleType);
+     decimal BasePrice,
+     VehicleType VehicleType);
